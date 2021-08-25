30dad03323940940b89b88876f297589b71a1909
GherasimGeorgian <pe.casaro10@gmail.com>
2021-08-25 14:26:38 +0300

    add header and imagegrid

---
+++ reduxsagapractice/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header/Header';
+import ImageGrid from './components/ImageGrid';
+function App() {
+  return (
+    <div>
+      <Header />
+      <ImageGrid />
+    </div>
+  );
+}
+
+export default App;
